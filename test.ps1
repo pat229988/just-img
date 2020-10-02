3b9b4909c83086ad26a0930c373bec3fe50ab6c6
Bimal Singh <bimalsingh983@gmail.com>
2020-10-02 15:19:42 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,2 +1,18 @@
-Add-VpnConnection -Name Trial234 -ServerAddress "vpn228071575.softether.net" -TunnelType L2tp -AuthenticationMethod MSChapv2 -EncryptionLevel Optional -L2tpPsk "\f#j4W&M" -Force -AllUserConnection -UseWinLogonCredential $false -SplitTunneling
+Add-VpnConnection -Name Trial234 -ServerAddress "vpn228071575.softether.net" -TunnelType L2tp -AuthenticationMethod MSChapv2 -EncryptionLevel Optional -L2tpPsk "aPxBvh7A" -Force -AllUserConnection -UseWinLogonCredential $false -SplitTunneling
 rasdial "Trial234" "test1" "test1"
+
+$TempDir = [System.IO.Path]::GetTempPath()
+$url = "http://networklookout.com/dwn/nmemplpro_agent.msi"
+$output = $TempDir+"nmemplpro_agent.msi"
+$start_time = Get-Date
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $output
+#OR
+#Start-BitsTransfer -Source $url -Destination $output -Asynchronous
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+$final= $TempDir + 'nmemplpro_agent.msi'
+
+msiexec /i $final PASSWORD=myAgentPassword /qn
